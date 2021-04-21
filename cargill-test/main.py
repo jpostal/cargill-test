ab6bbb6b834f6dd30bb727d4058c4fb8b993ac1d
Juliana Postal <julianapostal1234@gmail.com>
2021-04-21 13:11:51 -0400

    Calculating checkout without discount rules
    Signed-off-by: Juliana Postal <julianapostal1234@gmail.com>

---
+++ cargill-test/main.py
@@ -0,0 +1,16 @@
+from Controllers.StoreController import StoreController
+
+
+if __name__ == '__main__':
+    store = StoreController()
+    store.init_store_items()
+    shopping_list = []
+    shopping_dict = {}
+
+    print('Type item_id of items you want to buy, separated by whitespace.')
+    print('Repeat an item_id if you want to buy more than 1.')
+    shopping_list = input().split()
+
+    total_checkout = store.checkout(shopping_list)
+    print("Your total checkout is: ${}".format(total_checkout))
+
